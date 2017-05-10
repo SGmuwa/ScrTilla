33235b34d49e5146ea52e84b5239c994c6553900
SGmuwa <motherlode.muwa@gmail.com>
2017-05-10 23:12:50 +0300

    Теперь работает перехват Print Screen
    Следует навести порядок файлов, так как использовались разные версии перехвата hook

--- ScrTilla/Program.cs
+++ ScrTilla/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace ScrTilla
 {
@@ -10,6 +12,17 @@ namespace ScrTilla
     {
         static void Main(string[] args)
         {
+            PrtScr_Hook.PrintScreen += PrtScr_Hook_PrintScreen;
+            PrtScr_Hook.StartHook();
+            Application.Run();
+            PrtScr_Hook.StopHook();
+        }
+
+        private static void PrtScr_Hook_PrintScreen(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Print Screen");
         }
     }
+
+
 }
